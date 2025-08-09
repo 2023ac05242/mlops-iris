9b9d1eb2925f4dc7e67eef2a06e5d2489f4b4531
2023ac05242 <2023ac05242@wilp.bits-pilani.ac.in>
2025-08-09 20:21:47 +0530

    ♻️ Fix lint errors (remove unused numpy import)

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-#import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
